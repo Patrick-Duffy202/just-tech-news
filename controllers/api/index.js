ba07178dd46b916ba65df0dd60eb26a69957dd6c
Patrick Duffy <patrickduffy202@gmail.com>
2022-11-08 16:10:06 -0600

    added handlebars function

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
-const postRoutes = require('./post-routes');
+const postRoutes = require('./post-routes.js');
+const commentRoutes = require('./comment-routes.js');
 
+router.use('/comments', commentRoutes);
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
 
