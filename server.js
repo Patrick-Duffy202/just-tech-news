84056d4993e68e2d4a6247253cd477f005782ae0
Patrick Duffy <patrickduffy202@gmail.com>
2022-11-14 17:47:27 -0600

    updated everything

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 const helpers = require('./utils/helpers');
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 const exphbs = require('express-handlebars');
